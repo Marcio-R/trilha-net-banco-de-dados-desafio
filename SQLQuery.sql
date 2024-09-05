36c5f9416817522f3ea4497ef0ae8d63559f5e9d
Marcio Ronaldo Alves <marcio.ronaldoal@gmail.com>
2024-09-04 22:53:09 -0300

    Add files via upload

---
+++ SQLQuery.sql
@@ -0,0 +1,25 @@
+SELECT Nome,Ano FROM Filmes 
+
+SELECT Nome,Ano,Duracao FROM Filmes 
+
+SELECT nome, ano, duracao
+FROM Filmes
+WHERE nome LIKE '%De Volta para o Futuro%';
+
+SELECT nome, ano, duracao
+FROM Filmes
+WHERE ano = 1997
+
+SELECT nome, ano, duracao
+FROM Filmes
+WHERE ano > 2000;
+
+SELECT nome, ano, duracao
+FROM Filmes
+WHERE Duracao > 100 and Duracao < 150 order by Duracao ASC;
+
+SELECT ano, COUNT(*) AS quantidade_filmes
+FROM Filmes
+GROUP BY ano
+ORDER BY COUNT(*) DESC;
+
